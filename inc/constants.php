52b74eac67304b3d78ac4e72b175551dc816c9cb
GoetheWins <ray@easybitz.com>
2014-06-07 14:38:24 -0400

    cleanup

--- inc/constants.php
+++ inc/constants.php
@@ -7,11 +7,10 @@
 
 //##############################
 // Settings
-//define("JSONRPC_CONNECTIONSTRING_CC",		"https://A7sC5sYk1q4ef3XBoaqXYNq:9VnBT373isI1S3cA0cLqabs@5.153.60.162:8332"); //CoinCafe Custom RPC server at Softlayer
 define("RETURN_OUTPUTTYPE", 				"json");
-define("SUPPORT_EMAIL", 					"help@getcoincafe.com");
-define("EMAIL_ADMIN", 						"admin@getcoincafe.com");
-define("SERVER_IPADDRESS",					"37.58.86.163"); // internal 10.68.9.140 / ext 37.58.86.163
+define("SUPPORT_EMAIL", 					"help@gmail.com");
+define("EMAIL_ADMIN", 						"admin@gmail.com");
+define("SERVER_IPADDRESS",					"127.0.0.1"); //
 define("PASSWORD_ENCRYPT", 					""); //bcrypt 1
 define("__ROOT__", 							$_SERVER['DOCUMENT_ROOT']); 
 define("LOADCONTENT",						"/inc/loadcontent.php");
@@ -28,30 +27,30 @@ switch ($strServer){ // Server SiteWide Vars
 	case "dev":
 	define("MODE_UPGRADE", 				0);
 	define("DEBUGMODE", 				1);
-	define("WEBSITEURL", 				"local.apieasybitz");
+	define("WEBSITEURL", 				"local.ezapi");
 	define("WEBSITEFULLURL", 			"http://".WEBSITEURL); //"http://".WEBSITEURL ;
 	define("WEBSITEFULLURLHTTPS",		"http://".WEBSITEURL); //"https://".WEBSITEURL ;
 	define("DB_SERVER", 				"localhost");
 	define("DB_USER", 					"root");
 	define("DB_PASS", 					"littles");
 	define("DB_NAME", 					"easybitz_api");
-	define("JSONRPC_CONNECTIONSTRING_CC","https://61141261cRe2Epu0qOFU:L2iFnU14rf0r3v3r832W@37.58.86.163:8332"); //CoinCafe Custom RPC server at Softlayer
+	define("JSONRPC_CONNECTIONSTRING_CC","https://user:pass@37.58.86.163:8332"); //
 	define("JQUERYSRC",					'/js/jquery.min.js'); //latest jquery
 	define("JQUERYUISRC",				'/js/jqueryui.min.1.9.2.js'); //1.9.2 doesn't break blueimp upload and still allows drag and resize
 	break;
 
 	
-	case "sl": //
+	case "arthur": //
 	define("MODE_UPGRADE", 				0);
 	define("DEBUGMODE", 				0);
-	define("WEBSITEURL", 				"37.58.86.163");
+	define("WEBSITEURL", 				"127.0.0.1");
 	define("WEBSITEFULLURL", 			"http://".WEBSITEURL);
 	define("WEBSITEFULLURLHTTPS",		"https://".WEBSITEURL);
-	define("DB_SERVER", 				"localhost"); //10.68.9.138
+	define("DB_SERVER", 				"localhost"); //
 	define("DB_USER", 					"root");
-	define("DB_PASS", 					"2Epu0qOFUlLrJD832");
+	define("DB_PASS", 					"pass");
 	define("DB_NAME", 					"api"); //
-	define("JSONRPC_CONNECTIONSTRING_CC","http://61141261cRe2Epu0qOFU:L2iFnU14rf0r3v3r832W@127.0.0.1:8332"); //CoinCafe Custom RPC server at Softlayer
+	define("JSONRPC_CONNECTIONSTRING_CC","http://pass:user@127.0.0.1:8332"); //
 	define("JQUERYSRC",					'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
 	define("JQUERYUISRC",				'//ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js');//must upgrade to latest blueimp to use latest jqueryiu	
 	break;
