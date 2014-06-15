dc71fb31367e656733845c21dbe6f8a85c2717b2
Artur Schaback <skyzer@gmail.com>
2014-06-14 22:58:40 -0400

    fix merchant to api for $application_folder

--- codeigniter/index.php
+++ codeigniter/index.php
@@ -72,7 +72,7 @@ if (defined('ENVIRONMENT'))
  * NO TRAILING SLASH!
  *
  */
-	$application_folder = 'merchant';
+	$application_folder = 'api';
 
 /*
  * --------------------------------------------------------------------
