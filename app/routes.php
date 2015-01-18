fd557bf000f72cac7b927aa5896fe68c6e1d9183
Artur Schaback <skyzer@gmail.com>
2015-01-18 18:59:44 -0500

    added /api/ back to routes

--- app/routes.php
+++ app/routes.php
@@ -2,14 +2,14 @@
 Route::get('/', function()  {
 	return View::make('hello');
 });
-Route::get('{guid}/balance',                array('as' => 'balance',                'uses' => 'ApiController@balance'));
-Route::get('{guid}/address-balance',        array('as' => 'addressBalance',         'uses' => 'ApiController@addressBalance')); // not implemented
-Route::get('{guid}/validate-transaction',   array('as' => 'validateTransaction',    'uses' => 'ApiController@validateTransaction'));
-Route::get('{guid}/validate-address',       array('as' => 'validateAddress',        'uses' => 'ApiController@validateAddress'));
-Route::get('{guid}/new-address',            array('as' => 'newAddress',             'uses' => 'ApiController@newAddress'));
-Route::get('{guid}/tx-confirmations',       array('as' => 'txConfirmations',        'uses' => 'ApiController@txConfirmations'));
-Route::get('{guid}/payment',                array('as' => 'payment',                'uses' => 'ApiController@payment'));
+Route::get('api/{guid}/balance',                array('as' => 'balance',                'uses' => 'ApiController@balance'));
+Route::get('api/{guid}/address-balance',        array('as' => 'addressBalance',         'uses' => 'ApiController@addressBalance')); // not implemented
+Route::get('api/{guid}/validate-transaction',   array('as' => 'validateTransaction',    'uses' => 'ApiController@validateTransaction'));
+Route::get('api/{guid}/validate-address',       array('as' => 'validateAddress',        'uses' => 'ApiController@validateAddress'));
+Route::get('api/{guid}/new-address',            array('as' => 'newAddress',             'uses' => 'ApiController@newAddress'));
+Route::get('api/{guid}/tx-confirmations',       array('as' => 'txConfirmations',        'uses' => 'ApiController@txConfirmations'));
+Route::get('api/{guid}/payment',                array('as' => 'payment',                'uses' => 'ApiController@payment'));
 
-Route::get('callback',      array('as' => 'callback',       'uses' => 'ApiController@callback'));
-Route::get('receive',       array('as' => 'receive',        'uses' => 'ApiController@receive'));
-Route::get('blocknotify',   array('as' => 'blocknotify',    'uses' => 'ApiController@blocknotify'));
+Route::get('api/callback',      array('as' => 'callback',       'uses' => 'ApiController@callback'));
+Route::get('api/receive',       array('as' => 'receive',        'uses' => 'ApiController@receive'));
+Route::get('api/blocknotify',   array('as' => 'blocknotify',    'uses' => 'ApiController@blocknotify'));
